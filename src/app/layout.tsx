c44f1b7d7168a4ba0b7da098c75bdebf8c50db18
L-alam <lalam@bu.edu>
2026-04-30 11:44:58 -0400

    mobile dropdown fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#658C6E" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
